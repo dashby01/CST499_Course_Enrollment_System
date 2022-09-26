1c77f4252d96c5a048660c0cca916acd863782d4
David Ashby <david.ashby.02@gmail.com>
2022-09-26 09:51:31 -0700

    Add files via upload

---
+++ contactUs.php
@@ -0,0 +1,29 @@
+<?php
+require 'master.php';
+require 'footer.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="about-section">
+	<div style="text-align:center">
+	  <h1>Contact Us</h1>
+	  <p><p>For any technical issues or enhancement request. Please feel free to contact us.</p></p>
+
+		<h2> Tech Support</h2>
+		<div class="row">
+			  <img src="https://media-exp1.licdn.com/dms/image/C5603AQFyGGhvnhuDNw/profile-displayphoto-shrink_200_200/0/1516904397301?e=1669852800&v=beta&t=lJmjwMAenECRyH_W5I369WtNWqmmZRfNqnhjSBalR9k" alt="David" style="width:10%">
+			  <div class="container">
+				<h2>David Ashby</h2>
+				<p class="title">IT Support</p>
+				<p>david.ashby.02@gmail.com</p>
+				<p>https://github.com/dashby01</p>
+			</div>
+		</div>
+	</div>
+	</div>
+
+</body>  
+</html>
+
